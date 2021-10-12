8a576c9b08c1decff7cee92e9e23289f3f356ee0
Zhao KK <kikakkz@hotmail.com>
2021-10-12 06:27:35 +0000

    Add consul host

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-consul services register -address=${ENV_SERVICE_NAME}.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=mysql.npool.top -port=3306
+CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=${ENV_SERVICE_NAME}.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=mysql.npool.top -port=3306
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO REGISTER ME TO CONSUL"
   exit 1
