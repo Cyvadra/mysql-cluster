8ed897b57a2b7db94fb5fdc081db60f54316d221
Zhao KK <kikakkz@hotmail.com>
2021-10-12 08:17:19 +0000

    Remove service name

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=${ENV_SERVICE_NAME}.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=mysql.npool.top -port=3306
+CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=mysql.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=mysql.npool.top -port=3306
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO REGISTER ME TO CONSUL"
-  sleep 100000000000
+  exit 1
 fi
 
 /usr/local/bin/docker-entrypoint-inner.sh $@
