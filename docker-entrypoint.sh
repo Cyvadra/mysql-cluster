c8b37159ce35ef3b3039bafa2a5da21a5f5b2d10
Zhao KK <kikakkz@hotmail.com>
2021-10-12 07:12:04 +0000

    Test consul register

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 CONSUL_HTTP_ADDR=${ENV_CONSUL_HOST}:${ENV_CONSUL_PORT} consul services register -address=${ENV_SERVICE_NAME}.${ENV_CLUSTER_NAMESPACE}.svc.cluster.local -name=mysql.npool.top -port=3306
 if [ ! $? -eq 0 ]; then
   echo "FAIL TO REGISTER ME TO CONSUL"
-  exit 1
+  sleep 100000000000
 fi
 
 /usr/local/bin/docker-entrypoint-inner.sh $@
