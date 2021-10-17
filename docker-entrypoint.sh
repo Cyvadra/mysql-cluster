46215128b13c1e4047fa369e2f4636ec42ddf032
邓捷 <dj@lifesdk.com>
2021-10-17 14:53:57 +0800

    bugfix

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,34 +11,33 @@ chmod 600 ~/.my.cnf
 
 register_address="mysql.$ENV_CLUSTER_NAMESPACE.svc.cluster.local"
 
-do_registry () {
-  if [[ "$1"=="0" ]]; then
-    consul services register -address=$register_address -name=mysql.npool.top -port=3306
-  else
-    consul services register -address=$register_address -name=mysql-ro.npool.top -port=3306
-  fi
-}
-
-prev_state=-1
-
-while true; do
-  address_is_ro=$(mysql --defaults-extra-file=~/.my.cnf -e "SELECT @@global.super_read_only;" | tail -n1)
-  if [ ! $? -eq 0 ]; then
-    echo "whatever..."
-    break
-  fi
-  if [[ ! $address_is_ro -eq $prev_state ]]; then
-    do_registry $address_is_ro
+loop_registry () {
+  prev_state=-1
+  while true; do
+    address_is_ro=$(mysql --defaults-extra-file=~/.my.cnf -e "SELECT @@global.super_read_only;" | tail -n1)
     if [ ! $? -eq 0 ]; then
-      echo "\nFAIL TO REGISTER ME TO CONSUL\n"
+      echo "whatever... didn't register"
+      break
     fi
-    prev_state=$address_is_ro
-  fi
-  echo $address_is_ro
-  sleep 2
-done
-
+    if [[ ! $address_is_ro -eq $prev_state ]]; then
+      if [[ $address_is_ro -eq 1 ]]; then
+        consul services register -address=$register_address -name=mysql.npool.top -port=3306
+      else
+        consul services register -address=$register_address -name=mysql-ro.npool.top -port=3306
+      fi
+      if [ ! $? -eq 0 ]; then
+        echo "\nFAIL TO REGISTER ME TO CONSUL\n"
+      fi
+      prev_state=$address_is_ro
+    fi
+    echo $address_is_ro
+    sleep 2
+  done
+}
 
 
 
 /usr/local/bin/docker-entrypoint-inner.sh $@
+
+
+loop_registry&
